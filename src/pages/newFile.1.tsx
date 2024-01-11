32303ed53fa7b1cc6afd0248eac9a3e7e186159e
Jose Villa <villa.herrera.1994@gmail.com>
2024-01-11 13:52:57 -0300

    first commit

---
+++ src/pages/newFile.1.tsx
@@ -0,0 +1,21 @@
+import Layout from "../layouts/Layout.astro";
+
+<Fragment>
+<Layout title="Bienvenido a mi portafolio">
+<div class="container mx-auto mt-10 px-12 h-96 flex justify-center items-center">
+<div class="w-[80%] columns-2 flex">
+<div class="h-64 w-[30%] flex justify-center items-center">
+<div class="w-80 h-80 rounded-full flex justify-center items-center">
+<img class="w-80 h-80 object-cover rounded-full" src="/public/Jose Villa.webp" alt="" />
+</div>
+</div>
+<div class="w-[70%] h-64 px-5 py-2">
+<h1 class="text-6xl text-white">José Enrique Villa H.</h1>
+<h3 class="text-gray-300 text-3xl">Ingeniero de Sistemas</h3>
+</div>
+<p>pagina</p>
+</div>
+</div>
+</Layout>
+
+</Fragment>;
---
+++ src/pages/newFile.tsx
@@ -0,0 +1,8 @@
+import Layout from "../layouts/Layout.astro";
+
+<Fragment>
+<Layout title="Bienvenido a mi portafolio">
+<div class="container mx-auto px-12">hola mundo</div>
+</Layout>
+
+</Fragment>;
